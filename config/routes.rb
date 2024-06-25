276908992eedf0238f995bce1c9695f1e1fe86cf
FabioVV <fabio-varela@protonmail.com>
2024-06-25 16:12:02 -0300

    added my routes to the routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -49,3 +49,31 @@ end
 # And similar routes for creating, editing, updating, and deleting pages.
 # Viewing Routes: To see the routes created by Rails, run this command in your terminal:
 # rails routes
+
+
+
+
+
+
+
+
+# MY ROUTES BELOW (initial)
+
+
+# root GET    /                                                                                                 wigwam#home
+# book_pages GET    /books/:book_id/pages(.:format)                                                                   pages#index
+#            POST   /books/:book_id/pages(.:format)                                                                   pages#create
+# new_book_page GET    /books/:book_id/pages/new(.:format)                                                               pages#new
+# edit_book_page GET    /books/:book_id/pages/:id/edit(.:format)                                                          pages#edit
+#  book_page GET    /books/:book_id/pages/:id(.:format)                                                               pages#show
+#            PATCH  /books/:book_id/pages/:id(.:format)                                                               pages#update
+#            PUT    /books/:book_id/pages/:id(.:format)                                                               pages#update
+#            DELETE /books/:book_id/pages/:id(.:format)                                                               pages#destroy
+#      books GET    /books(.:format)                                                                                  books#index
+#            POST   /books(.:format)                                                                                  books#create
+#   new_book GET    /books/new(.:format)                                                                              books#new
+#  edit_book GET    /books/:id/edit(.:format)                                                                         books#edit
+#       book GET    /books/:id(.:format)                                                                              books#show
+#            PATCH  /books/:id(.:format)                                                                              books#update
+#            PUT    /books/:id(.:format)                                                                              books#update
+#            DELETE /books/:id(.:format)   
