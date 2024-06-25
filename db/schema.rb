604ba558ed2ed5af86ff11fdb572b04d1c43a2b0
FabioVV <fabio-varela@protonmail.com>
2024-06-25 10:24:30 -0300

    added initial modelling of books and pages

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_21_172954) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_25_125823) do
+  create_table "books", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_books_on_user_id"
+  end
+
+  create_table "pages", force: :cascade do |t|
+    t.integer "book_id", null: false
+    t.text "context"
+    t.integer "number"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_pages_on_book_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", default: "", null: false
     t.string "first_name", default: "", null: false
@@ -27,4 +44,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_21_172954) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "books", "users"
+  add_foreign_key "pages", "books"
 end
