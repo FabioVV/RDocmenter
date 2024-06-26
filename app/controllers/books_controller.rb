1d21d82f602c4e537aebf909f7a9fe9c69e06e4e
FabioVV <fabio-varela@protonmail.com>
2024-06-26 10:59:21 -0300

    added create, list of books, setup a lot of the books controller groundwork, minor changes on the home page, book _form page and _nav

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,51 @@
 class BooksController < ApplicationController
+    before_action :set_book, only: [:show, :edit, :update, :destroy]
+
+    def index 
+        @any_books = current_user.books.any?
+        @books = current_user.books
+    end
+
+    def show
+    end
 
     def new 
         @book = Book.new
     end
 
+    def create 
+        @book = Book.new(book_params)
+        @book.user = current_user
+
+        if @book.save
+            flash[:notice] = "Book/Document created successfully!"
+            redirect_to books_path
+
+        else
+            render 'new', status: :unprocessable_entity
+        end
+
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
 
     private
+
+    def set_book
+        @book = Book.find(params[:id])
+    end
+
+    def book_params
+        params.require(:book).permit(:title, :subtitle)
+    end
+
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
     belongs_to :user
     has_many :pages, dependent: :destroy
+
+    validates :title, presence: true, 
+    length: {minimum:3, maximum: 100}
+
+    validates :subtitle, presence: true, 
+    length: {minimum:3, maximum: 100}
 end
