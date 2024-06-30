c90011c53bdbd84e953d39b1a49ae6868b6c68d2
Fabio Varela <fabio-varela@protonmail.com>
2024-06-30 15:52:41 -0300

    added validation to only allow images on book cover, added preview of image upload on create new book

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,9 +4,26 @@ class Book < ApplicationRecord
 
     has_one_attached :cover_image
 
+    validate :file_format
+
     validates :title, presence: true, 
     length: {minimum:3, maximum: 100}
 
     validates :subtitle, presence: true, 
     length: {minimum:3, maximum: 100}
+
+
+    private 
+
+    def file_format
+        return unless cover_image.attached? && !image?
+
+        errors.add(:cover_image, 'Cover of book/document must be an image')
+        cover_image.purge
+    end
+
+    def image?
+        #["image/jpeg", "image/png", "image/gif", "image/webp"].include?(cover_image.content_type)
+        cover_image.content_type.in?(["image/jpeg", "image/png", "image/gif", "image/webp"])
+    end
 end
