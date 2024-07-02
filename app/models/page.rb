09c0a0a857bb8e594a0a52947a67d29ea9621f27
FabioVV <fabio-varela@protonmail.com>
2024-07-02 15:08:32 -0300

    WIP

--- app/models/page.rb
+++ app/models/page.rb
@@ -4,4 +4,18 @@ class Page < ApplicationRecord
   has_many_attached :images
   has_one_attached :page_image, dependent: :purge_later
 
+  # cattr_accessor :preview_renderer do
+  #   renderer = Redcarpet::Render::HTML.new(ActionText::Markdown::DEFAULT_RENDERER_OPTIONS)
+  #   Redcarpet::Markdown.new(renderer, ActionText::Markdown::DEFAULT_MARKDOWN_EXTENSIONS)
+  # end
+
+  # def html_preview
+  #   preview_renderer.render(body_preview)
+  # end
+
+  private
+    def body_preview
+      content.to_s.first(1024)
+    end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_02_133230) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_02_175748) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_07_02_133230) do
     t.datetime "updated_at", null: false
     t.string "subtitle"
     t.boolean "is_active"
+    t.string "author"
     t.index ["user_id"], name: "index_books_on_user_id"
   end
 
