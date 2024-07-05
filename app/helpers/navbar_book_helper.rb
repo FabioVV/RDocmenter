d2c8efecf9e1fedb2168f46e0dd8184440424696
FabioVV <fabio-varela@protonmail.com>
2024-07-05 14:25:53 -0300

    add page_id to nav helper, some changes in style

--- app/helpers/navbar_book_helper.rb
+++ app/helpers/navbar_book_helper.rb
@@ -2,9 +2,10 @@ module NavbarBookHelper
     def navbar_book_options(options = {})
         should_return_to = options[:should_return_to] || :back
         book =  options[:book]
+        page_id =  options[:page_id]
         page = options.fetch(:page, false)
         hide_input =  options.fetch(:hide_input, false)
 
-        render 'layouts/nav_book', should_return_to: should_return_to, page: page, book: book, hide_input: hide_input
+        render 'layouts/nav_book', should_return_to: should_return_to, page: page, page_id:page_id, book: book, hide_input: hide_input
     end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,9 +1,15 @@
 module PagesHelper
+
     def toolbar_options(edit: false)
         if edit
             render 'toolbar/toolbar_edit'
         else
             render 'toolbar/toolbar_show'
         end
+    end
+
+    def word_count(content)
+        return unless !content.blank?
+        pluralize number_with_delimiter(content.split.size), "word"
     end
 end
