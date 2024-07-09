62689a3677d11fe02bffe1a1da205bdc25ae1ed2
Fabio Varela <fabio-varela@protonmail.com>
2024-07-08 21:42:59 -0300

    lots of style changes, different rendering place of toolbox, fix <br> on markdown editor

--- vendor/javascript/documenter.js
+++ vendor/javascript/documenter.js
@@ -5,11 +5,18 @@ class Documenter extends HTMLElement{
 
     connectedCallback(){
 
-
         const doc_content = document.createElement('div')
         doc_content.classList.add("doc-menter-content"),
         doc_content.setAttribute('contenteditable', !0)
         this.appendChild(doc_content)
+
+        doc_content.addEventListener('keydown', (event) => {
+            if(event.key == 'Enter'){
+                event.preventDefault()
+                
+                document.execCommand('insertLineBreak');
+            }
+        })
 
         // this.querySelector('#unordered-list').addEventListener('click', () => {
         //     document.execCommand('insertUnorderedList');
