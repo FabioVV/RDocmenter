130528f6d13afdfae4d98794b692c033d4170c62
FabioVV <fabio-varela@protonmail.com>
2024-07-17 12:05:55 -0300

    add theme switching (dark and light), some small style changes

--- app/javascript/controllers/webtheme_controller.js
+++ app/javascript/controllers/webtheme_controller.js
@@ -2,7 +2,32 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
     
-    changeTheme() {
-        document.body.classList.toggle("dark-theme");
+    connect(){
+        this.applyTheme()
+    }
+
+    changeTheme(){
+        document.documentElement.classList.toggle('dark-theme')
+        this.saveTheme()
+    }
+
+    saveTheme(){
+        const isDarkTheme = document.documentElement.classList.contains('dark-theme')
+        document.cookie = `theme=${isDarkTheme ? 'dark' : 'light'}; path=/; max-age=31536000`; // Expires in 1 year
+    }
+
+    applyTheme(){
+        const theme = this.getCookie('theme')
+        if(theme == 'dark'){
+            document.documentElement.classList.add('dark-theme')
+        } else {
+            document.documentElement.classList.remove('dark-theme')
+        }
+    }
+
+    getCookie(name){
+        const value = `; ${document.cookie}`
+        const parts = value.split(`; ${name}=`)
+        if(parts.length == 2) return parts.pop().split(';').shift()
     }
 }
