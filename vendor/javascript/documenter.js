cecb8a655a53efb95a1517e14de948166d0e3327
Fábio Varela <fabio-varela@protonmail.com>
2024-07-21 20:43:04 -0300

    add user img to navbar, style changes, add new code button for markdown toolbar

--- vendor/javascript/documenter.js
+++ vendor/javascript/documenter.js
@@ -19,19 +19,20 @@ class Documenter extends HTMLElement {
         this.appendChild(this.contentDiv)
         this.contentDiv.innerHTML = parseMarkdown(this.inputHidden.value)
 
-        this.contentDiv.addEventListener('keydown', this.handleKeyDown.bind(this));
+        this.contentDiv.addEventListener('keydown', this.handleKeyDown.bind(this))
         this.contentDiv.addEventListener('input', this.handleInput.bind(this))
-        this.contentDiv.addEventListener('blur', this.handleBlur.bind(this));
-        this.contentDiv.addEventListener('focus', this.handleFocus.bind(this));
+        this.contentDiv.addEventListener('blur', this.handleBlur.bind(this))
+        this.contentDiv.addEventListener('focus', this.handleFocus.bind(this))
 
-        document.getElementById('markdown-file').addEventListener('change', this.uploadImage.bind(this));
-        document.querySelector('.image.editor-button').addEventListener('click', this.handleClickOnFileField.bind(this));
-        document.querySelector('.bold.editor-button').addEventListener('click', this.insertBold.bind(this));
-        document.querySelector('.italic.editor-button').addEventListener('click', this.insertItalic.bind(this));
-        document.querySelector('.header.editor-button').addEventListener('click', this.insertHeader.bind(this));
-        document.querySelector('.listul.editor-button').addEventListener('click', this.insertUnorderedList.bind(this));
-        document.querySelector('.listol.editor-button').addEventListener('click', this.insertOrderedList.bind(this));
-        document.querySelector('.link.editor-button').addEventListener('click', this.insertLink.bind(this));
+        document.getElementById('markdown-file').addEventListener('change', this.uploadImage.bind(this))
+        document.querySelector('.image.editor-button').addEventListener('click', this.handleClickOnFileField.bind(this))
+        document.querySelector('.bold.editor-button').addEventListener('click', this.insertBold.bind(this))
+        document.querySelector('.italic.editor-button').addEventListener('click', this.insertItalic.bind(this))
+        document.querySelector('.header.editor-button').addEventListener('click', this.insertHeader.bind(this))
+        document.querySelector('.listul.editor-button').addEventListener('click', this.insertUnorderedList.bind(this))
+        document.querySelector('.listol.editor-button').addEventListener('click', this.insertOrderedList.bind(this))
+        document.querySelector('.code.editor-button').addEventListener('click', this.insertCode.bind(this))
+        document.querySelector('.link.editor-button').addEventListener('click', this.insertLink.bind(this))
 
     }
 
@@ -40,15 +41,15 @@ class Documenter extends HTMLElement {
     }
 
     handleClickOnFileField(){
-        document.getElementById('markdown-file').click();
+        document.getElementById('markdown-file').click()
     }
 
     handleMarkdown(){
         let contentElement = this.contentDiv
 
-        const scrollPosition = window.scrollY || window.pageYOffset;
+        const scrollPosition = window.scrollY || window.pageYOffset
 
-        const content = contentElement.textContent;
+        const content = contentElement.textContent
         const restore = this.saveCaretPosition(contentElement)
         const parsedContent = parseMarkdown(content)
 
@@ -57,7 +58,7 @@ class Documenter extends HTMLElement {
 
         restore()
 
-        window.scrollTo(0, scrollPosition);
+        window.scrollTo(0, scrollPosition)
     }
 
     handleInput(event) {
@@ -74,70 +75,75 @@ class Documenter extends HTMLElement {
             event.preventDefault()
             document.execCommand('insertLineBreak')
 
-            // const selection = window.getSelection();
-            // const range = selection.getRangeAt(0);
-            // const currentNode = range.startContainer;
-            // const textBeforeCaret = currentNode.textContent.substring(0, range.startOffset);
-            // const linesBefore = textBeforeCaret.split('\n');
-            // const currentLine = linesBefore[linesBefore.length - 1];
+            // const selection = window.getSelection()
+            // const range = selection.getRangeAt(0)
+            // const currentNode = range.startContainer
+            // const textBeforeCaret = currentNode.textContent.substring(0, range.startOffset)
+            // const linesBefore = textBeforeCaret.split('\n')
+            // const currentLine = linesBefore[linesBefore.length - 1]
         
             // let newLine = ''
             // if (currentLine.match(/^\d+\. /)) {
-            //   newLine = `\n${parseInt(currentLine, 10) + 1}. `;
+            //   newLine = `\n${parseInt(currentLine, 10) + 1}. `
             // } else if (currentLine.match(/^- /)) {
-            //   newLine = `\n- `;
+            //   newLine = `\n- `
             // } else {
-            //     document.execCommand('insertLineBreak');
+            //     document.execCommand('insertLineBreak')
             // }
 
-            // range.insertNode(document.createTextNode(newLine));
+            // range.insertNode(document.createTextNode(newLine))
             // window.getSelection().removeAllRanges()
 
         }
     }
 
     handleBlur(event) {
-        this.saveCaret = this.saveCaretPosition(this.contentDiv);
+        this.saveCaret = this.saveCaretPosition(this.contentDiv)
         window.getSelection().removeAllRanges()
     }
 
     handleFocus(event) {
         if (this.saveCaret) {
-            this.saveCaret();
+            this.saveCaret()
         }
     }
 
     insertHeader() {
-        this.insertAtCaret('# ');
+        this.insertAtCaret('# ')
     }
 
     insertBold() {
-        this.insertAtCaret('****');
+        this.insertAtCaret('****')
     }
 
     insertItalic() {
-        this.insertAtCaret('**');
+        this.insertAtCaret('**')
     }
 
     insertUnorderedList() {
-        this.insertAtCaret('- ');
+        this.insertAtCaret('- ')
     }
 
     insertOrderedList() {
-        this.insertAtCaret('1. ');
+        this.insertAtCaret('1. ')
     }
 
     insertLink() {
-        this.insertAtCaret(`[title](url)`);
+        this.insertAtCaret(`[title](url)`)
+    }
+
+    insertCode(){
+        const CODE = "\n``"
+        this.insertAtCaret(`${CODE}`)
     }
 
     insertImage(url, filename) {
-        const markdownImage = `![${filename}](${url})`;
-        this.insertAtCaret(markdownImage);
+        const markdownImage = `![${filename}](${url})`
+        this.insertAtCaret(markdownImage)
     }
 
     async uploadImage(event) {
-        const file = event.target.files[0];
+        const file = event.target.files[0]
     
         if (file) {
           const formData = new FormData()
@@ -153,11 +159,11 @@ class Documenter extends HTMLElement {
                     'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
                 },
                 body: formData
-            });
+            })
     
             if(response.ok){
               const { url, filename } = await response.json()
-              this.insertImage(url, filename);
+              this.insertImage(url, filename)
             } else {
               console.log('Image tag could not be generated.')
             }
@@ -172,54 +178,55 @@ class Documenter extends HTMLElement {
     }
 
     saveCaretPosition(context){
-        let selection = window.getSelection();
-        let range = selection.getRangeAt(0);
-        range.setStart(  context, 0 );
-        let len = range.toString().length;
+        let selection = window.getSelection()
+        let range = selection.getRangeAt(0)
+        range.setStart(  context, 0 )
+        let len = range.toString().length
     
         return function restore(){
-            let pos = getTextNodeAtPosition(context, len);
-            selection.removeAllRanges();
-            let range = new Range();
-            range.setStart(pos.node ,pos.position);
-            selection.addRange(range);
+            let pos = getTextNodeAtPosition(context, len)
+            selection.removeAllRanges()
+            let range = new Range()
+            range.setStart(pos.node ,pos.position)
+            selection.addRange(range)
     
         }
     }
 
     insertAtCaret(text){
-        this.contentDiv.focus();
+        this.contentDiv.focus()
 
-        const selection = window.getSelection();
-        if (!selection.rangeCount) return;
+        const selection = window.getSelection()
+        if (!selection.rangeCount) return
 
-        const range = selection.getRangeAt(0);
+        const range = selection.getRangeAt(0)
 
         if (!this.contentDiv.contains(range.commonAncestorContainer)) {
-            return;
+            return
         }
 
-        range.deleteContents();
+        range.deleteContents()
 
-        const textNode = document.createTextNode(text);
-        range.insertNode(textNode);    
+        const textNode = document.createTextNode(text)
+        range.insertNode(textNode)    
 
         this.handleMarkdown()
     }
 
     #cleanEventListeners(){
-        this.contentDiv.removeEventListener('keydown', this.handleKeyDown.bind(this));
-        this.contentDiv.removeEventListener('input', this.handleInput.bind(this));
-        this.contentDiv.removeEventListener('blur', this.handleBlur.bind(this));
-        this.contentDiv.removeEventListener('focus', this.handleFocus.bind(this));
+        this.contentDiv.removeEventListener('keydown', this.handleKeyDown.bind(this))
+        this.contentDiv.removeEventListener('input', this.handleInput.bind(this))
+        this.contentDiv.removeEventListener('blur', this.handleBlur.bind(this))
+        this.contentDiv.removeEventListener('focus', this.handleFocus.bind(this))
         document.getElementById('markdown-file').removeEventListener('change', this.uploadImage.bind(this))
-        document.querySelector('.image.editor-button').removeEventListener('click', this.handleClickOnFileField.bind(this));
-        document.querySelector('.bold.editor-button').removeEventListener('click', this.insertBold.bind(this));
-        document.querySelector('.italic.editor-button').removeEventListener('click', this.insertItalic.bind(this));
-        document.querySelector('.header.editor-button').removeEventListener('click', this.insertHeader.bind(this));
-        document.querySelector('.listul.editor-button').removeEventListener('click', this.insertUnorderedList.bind(this));
-        document.querySelector('.listol.editor-button').removeEventListener('click', this.insertOrderedList.bind(this));
-        document.querySelector('.link.editor-button').removeEventListener('click', this.insertLink.bind(this));
+        document.querySelector('.image.editor-button').removeEventListener('click', this.handleClickOnFileField.bind(this))
+        document.querySelector('.bold.editor-button').removeEventListener('click', this.insertBold.bind(this))
+        document.querySelector('.italic.editor-button').removeEventListener('click', this.insertItalic.bind(this))
+        document.querySelector('.header.editor-button').removeEventListener('click', this.insertHeader.bind(this))
+        document.querySelector('.listul.editor-button').removeEventListener('click', this.insertUnorderedList.bind(this))
+        document.querySelector('.listol.editor-button').removeEventListener('click', this.insertOrderedList.bind(this))
+        document.querySelector('.code.editor-button').removeEventListener('click', this.insertCode.bind(this))
+        document.querySelector('.link.editor-button').removeEventListener('click', this.insertLink.bind(this))
     }
 
 }
@@ -227,22 +234,22 @@ class Documenter extends HTMLElement {
 
 // Helper of saveCaretPosition
 function getTextNodeAtPosition(root, index){
-    const NODE_TYPE = NodeFilter.SHOW_TEXT;
+    const NODE_TYPE = NodeFilter.SHOW_TEXT
     let treeWalker = document.createTreeWalker(root, NODE_TYPE, function next(elem) {
         if(index > elem.textContent.length){
-            index -= elem.textContent.length;
+            index -= elem.textContent.length
             return NodeFilter.FILTER_REJECT
         }
-        return NodeFilter.FILTER_ACCEPT;
-    });
-    let c = treeWalker.nextNode();
+        return NodeFilter.FILTER_ACCEPT
+    })
+    let c = treeWalker.nextNode()
 
     return {
         node: c? c: root,
         position: index
-    };
+    }
 }
 
 
 
-customElements.define('doc-menter', Documenter);
+customElements.define('doc-menter', Documenter)
